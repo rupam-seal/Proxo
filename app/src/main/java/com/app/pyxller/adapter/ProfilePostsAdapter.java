7c4f22553f4fdf9628c5d71402a7e3cd26d89fcd
rupam-seal <rupam.x.seal@gmail.com>
2023-04-02 12:29:15 +0530

    ✨feat(ProfilePostsAdapter): add posts number of likes

--- app/src/main/java/com/app/pyxller/adapter/ProfilePostsAdapter.java
+++ app/src/main/java/com/app/pyxller/adapter/ProfilePostsAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,6 +13,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.app.pyxller.R;
 import com.app.pyxller.model.Posts;
 import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -40,6 +46,10 @@ public class ProfilePostsAdapter extends RecyclerView.Adapter<ProfilePostsAdapte
 
         Glide.with(mContext).load(model.getPostImage()).into(holder.postImage);
 
+        if (model.getPostid() != null){
+            numLikes(holder.profile_likes_num, model.getPostid());
+        }
+
     }
 
     @Override
@@ -50,10 +60,27 @@ public class ProfilePostsAdapter extends RecyclerView.Adapter<ProfilePostsAdapte
     public static class ProfilePostsViewHolder extends RecyclerView.ViewHolder {
 
         private ImageView postImage;
+        private TextView profile_likes_num;
 
         public ProfilePostsViewHolder(@NonNull View itemView) {
             super(itemView);
             postImage = itemView.findViewById(R.id.p_postsItem);
+            profile_likes_num = itemView.findViewById(R.id.profile_likes_num);
         }
+    }
+
+    private void numLikes(TextView numLikes, String postid) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Likes").child(postid);
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                numLikes.setText(snapshot.getChildrenCount() + "");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
